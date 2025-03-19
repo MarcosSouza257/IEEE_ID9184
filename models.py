1c7308a1962487b1e97f695d1bec68a67185b582
Marcos Roberto Souza <marcosrsouza@edu.unisinos.br>
2025-03-19 12:08:47 -0300

    add save_artifacts model 3

--- models.py
+++ models.py
@@ -16,16 +16,17 @@ import utils
 def model_1(stock, df, target_column, num_combination, save_artifacts=False, learning_rate=0.001, epochs=50, batch_size=32):
     """
     Treina um modelo LSTM e, opcionalmente, salva o modelo treinado, scaler_X e scaler_y na pasta do ativo.
+    Opcionalmente, salva o modelo treinado e os scalers (scaler_X e scaler_y) na pasta do ativo.
 
     Parâmetros:
         stock (str): Nome da ação.
         df (pd.DataFrame): DataFrame com os dados.
         target_column (str): Nome da coluna de destino (target).
         num_combination (int): Identificador da combinação de hiperparâmetros.
-        learning_rate (float): Taxa de aprendizado do otimizador Adam. Padrão: 0.001
+        save_artifacts (bool): Se True, salva o modelo e os scalers. Padrão: False
+        learning_rate (float): Taxa de aprendizado do otimizador. Padrão: 0.001
         epochs (int): Número de épocas para treinamento. Padrão: 50
-        batch_size (int): Tamanho do batch para treinamento. Padrão: 32
-        save_artifacts (bool): Se True, salva o modelo e os scalers. Padrão: True
+        batch_size (int): Tamanho do batch para treinamento. Padrão: 32        
 
     Retorna:
         dict: Dicionário com métricas de desempenho (Loss, MSE, RMSE, MAE, MAPE, R²).
@@ -123,16 +124,17 @@ def model_1(stock, df, target_column, num_combination, save_artifacts=False, lea
 def model_2(stock, df, target_column, num_combination, save_artifacts=False, learning_rate=0.001, dropout_rate=0.03, epochs=50, batch_size=32):
     """
     Treina um modelo LSTM com múltiplas camadas e Dropout e salva o modelo treinado, scaler_X e scaler_y na pasta do ativo.
+    Opcionalmente, salva o modelo treinado e os scalers (scaler_X e scaler_y) na pasta do ativo.
 
     Parâmetros:
         stock (str): Nome da ação.
         df (pd.DataFrame): DataFrame com os dados.
         target_column (str): Nome da coluna de destino (target).
         num_combination (int): Identificador da combinação de hiperparâmetros.
-        learning_rate (float): Taxa de aprendizado do otimizador Adam. Padrão: 0.001
-        dropout_rate (float): Taxa de dropout para regularização. Padrão: 0.03
+        save_artifacts (bool): Se True, salva o modelo e os scalers. Padrão: False
+        learning_rate (float): Taxa de aprendizado do otimizador. Padrão: 0.001
         epochs (int): Número de épocas para treinamento. Padrão: 50
-        batch_size (int): Tamanho do batch para treinamento. Padrão: 32
+        batch_size (int): Tamanho do batch para treinamento. Padrão: 32        
 
     Retorna:
         dict: Dicionário com métricas de desempenho (Loss, MSE, RMSE, MAE, MAPE, R²).
@@ -197,7 +199,6 @@ def model_2(stock, df, target_column, num_combination, save_artifacts=False, lea
 
     if save_artifacts:
         # Salvando os scalers na pasta do ativo
-        # Salvando os scalers na pasta do ativo
         joblib.dump(scaler_X, os.path.join(stock_dir, f"model_2_comb_{num_combination}_scaler_X.pkl"))
         joblib.dump(scaler_y, os.path.join(stock_dir, f"model_2_comb_{num_combination}_scaler_y.pkl"))
         # Salvando o modelo treinado na pasta do ativo
@@ -233,42 +234,51 @@ def model_2(stock, df, target_column, num_combination, save_artifacts=False, lea
     return metrics_dict
 
 # Modelo 3
-def model_3(stock, df, target_column, num_combination, learning_rate=0.001, epochs=50, batch_size=32):
+def model_3(stock, df, target_column, num_combination, save_artifacts=False, learning_rate=0.001, epochs=50, batch_size=32):
     """
     Treina um modelo de Rede Neural Simples (MLP) e retorna as métricas de desempenho.
+    Opcionalmente, salva o modelo treinado e os scalers (scaler_X e scaler_y) na pasta do ativo.
 
     Parâmetros:
+        stock (str): Nome da ação.
         df (pd.DataFrame): DataFrame com os dados.
         target_column (str): Nome da coluna de destino (target).
+        num_combination (int): Identificador da combinação de hiperparâmetros.
+        save_artifacts (bool): Se True, salva o modelo e os scalers. Padrão: False
         learning_rate (float): Taxa de aprendizado do otimizador. Padrão: 0.001
         epochs (int): Número de épocas para treinamento. Padrão: 50
-        batch_size (int): Tamanho do batch para treinamento. Padrão: 32
+        batch_size (int): Tamanho do batch para treinamento. Padrão: 32        
 
     Retorna:
         dict: Dicionário com métricas de desempenho (Loss, MSE, RMSE, MAE, MAPE, R²).
     """
 
+    # Criando diretório específico para o ativo dentro de OUTPUT_DIR
+    stock_dir = os.path.join(OUTPUT_DIR, stock)
+    os.makedirs(stock_dir, exist_ok=True)
+
     # Separando as variáveis independentes (X) e dependente (y)
     X = df.drop(columns=[target_column]).values
-    y = df[target_column].values
+    y = df[target_column].values.reshape(-1, 1) #Reshape para scaler funcionar corretamente
 
     # Dividindo os dados em 80% para treino e 20% para teste
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
 
     # Escalonando os dados
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    scaler_X = MinMaxScaler(feature_range=(0, 1))
+    X_train_scaled = scaler_X.fit_transform(X_train)
+    X_test_scaled = scaler_X.transform(X_test)
 
-    # Salvando o scaler
-    joblib.dump(scaler, f"{OUTPUT_DIR}/{stock}_model_3_comb_{num_combination}_scaler.pkl")
+    scaler_y = MinMaxScaler(feature_range=(0,1))
+    y_train_scaled = scaler_y.fit_transform(y_train)
+    y_test_scaled = scaler_y.transform(y_test)
 
     # Criando o modelo MLP
     model = Sequential([
-        Input(shape=(X.shape[1],)),   # Ajustando o input
+        Input(shape=(X.shape[1],)),  # Ajustando o input
         Dense(32, activation='relu'),  # Primeira camada oculta
-        Dense(8, activation='relu'),   # Segunda camada oculta
-        Dense(1)                       # Camada de saída
+        Dense(8, activation='relu'),  # Segunda camada oculta
+        Dense(1)  # Camada de saída
     ])
 
     # Compilando o modelo
@@ -284,21 +294,28 @@ def model_3(stock, df, target_column, num_combination, learning_rate=0.001, epoc
 
     # Treinamento do modelo
     history = model.fit(
-        X_train_scaled, y_train,
+        X_train_scaled, y_train_scaled,
         epochs=epochs,
         batch_size=batch_size,
-        validation_data=(X_test_scaled, y_test),
+        validation_data=(X_test_scaled, y_test_scaled),
         callbacks=[early_stopping],
         verbose=1
     )
-    # Salvando o modelo treinado
-    save_model(model, f"{OUTPUT_DIR}/{stock}_model_3_comb_{num_combination}.h5")
+
+    if save_artifacts:
+        # Salvando os scalers
+        joblib.dump(scaler_X, os.path.join(stock_dir, f"model_3_comb_{num_combination}_scaler_X.pkl"))
+        joblib.dump(scaler_y, os.path.join(stock_dir, f"model_3_comb_{num_combination}_scaler_y.pkl"))
+        # Salvando o modelo treinado
+        model_path = os.path.join(stock_dir, f"model_3_comb_{num_combination}.h5")
+        save_model(model, model_path)
 
     # Obtendo a última loss registrada
     loss = history.history['loss'][-1]
 
     # Previsão no conjunto de teste
-    y_pred = model.predict(X_test_scaled).flatten()
+    y_pred_scaled = model.predict(X_test_scaled).flatten()
+    y_pred = scaler_y.inverse_transform(y_pred_scaled.reshape(-1, 1)).flatten()
 
     # Cálculo de métricas
     mse = mean_squared_error(y_test, y_pred)
